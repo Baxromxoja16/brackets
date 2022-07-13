6c5875a9914ce026c01e8579150cffac3dcc9cfa
Baxromxoja16 <aglamof@mail.ru>
2022-07-13 16:29:28 +0500

    feat: no complated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = function check(str, brac) {
   let answ = "";
   brac.forEach((element) => {
     answ = element[0] + element[1];
   });
+  console.log(answ === str);
   return answ === str;
 };
