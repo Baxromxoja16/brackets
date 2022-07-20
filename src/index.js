ecd8e5861b75f8e49841e32c08bf9045200a6e57
Baxromxoja16 <aglamof@mail.ru>
2022-07-20 10:57:19 +0500

    feat: problem solved

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 module.exports = function check(str, brac) {
-  let answ = "";
-  brac.forEach((element) => {
-    answ = element[0] + element[1];
-  });
-  console.log(answ === str);
-  return answ === str;
+  const bracArr = brac.map(currentValue => currentValue.join(''));
+  for (let i = 0; i < bracArr.length; i++) {
+    if (str.includes(bracArr[i])) {
+      str = str.replace(bracArr[i], "")
+      console.log(str);
+      i = -1
+    }
+  }
+  return !str
 };
